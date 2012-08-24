d954cff2cba8b2ada12a7b0ff7f15982cbfb4487
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2012-08-24 00:06:01 -0700

    Escape \n in input, expected and result strings in tests.

--- filter.t.c
+++ filter.t.c
@@ -5,6 +5,26 @@
 
 #include "filter.h"
 
+char * quote(char const * s) {
+	int i = 0;
+	int o = 0;
+	char * r = malloc(strlen(s) * 2 + 1);
+	while (s[i] != '\0') {
+		switch(s[i]) {
+		case '\n':
+			r[i+o] = '\\';
+			r[i+o+1] = 'n';
+			++o;
+			break;
+		default:
+			r[i+o] = s[i];
+		}
+		++i;
+	}
+	r[i+o] = '\0';
+	return r;
+}
+
 int main(void) {
 
 	int test = 0;
@@ -23,21 +43,36 @@ int main(void) {
 	for (test = 0; test < test_n; ++test) {
 		char *result = NULL;
 
+		char *qi;
+		char *qe;
+		char *qr;
+
 		/* Test information */
-		printf("  [%4d] Test %3u: decode_alloc(\"%s\") should return \"%s\"\n", __LINE__, test, input[test], expected[test]);
+		qi = quote(input[test]);
+		qe = quote(expected[test]);
+		printf("  [%4d] Test %3u: decode_alloc(\"%s\") should return \"%s\"\n", __LINE__, test, qi, qe);
 
 		/* Test execution */
 		result = decode_alloc(input[test]);
 
 		/* Test assertions */
+		qr = quote(result);
 		assert(result != NULL);
 		if (strcmp(result, expected[test]) != 0) {
-			printf("        FAILED: Instead, got \"%s\"\n", result);
+			printf("        FAILED: Instead, got \"%s\"\n", qr);
+
 			free(result);
+			free(qi);
+			free(qe);
+			free(qr);
 			return EXIT_FAILURE;
 		};
 
 		free(result);
+		free(qi);
+		free(qe);
+		free(qr);
+
 	}
 
 	return EXIT_SUCCESS;
